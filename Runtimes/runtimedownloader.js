71da4d61c46992ab7dcadf5398d1fa80d7bf1db8
Dylan Z. Baker <dbaker5525@gmail.com>
2026-06-08 20:39:49 -0400

    Replacing decompression package

--- Runtimes/runtimedownloader.js
+++ Runtimes/runtimedownloader.js
@@ -1,46 +1,60 @@
 // Copyright (c) 2019-2026 Five Squared Interactive. All rights reserved.
 
 const { DownloaderHelper } = require('node-downloader-helper');
-const decompress = require("decompress");
-const fs = require("fs");
+const AdmZip = require('adm-zip');
+const fs = require('fs');
+const path = require('path');
 
 /**
- * @function DownloadFiles Download Runtime Riles.
+ * @function ExtractZip Extract a zip file to a destination, handling
+ * explicit directory entries that some Unity-produced zips contain.
+ * @param {string} zipPath Path to the zip file.
+ * @param {string} destination Destination directory.
+ */
+function ExtractZip(zipPath, destination) {
+    // Ensure destination exists
+    fs.mkdirSync(destination, { recursive: true });
+
+    const zip = new AdmZip(zipPath);
+    // Second arg "true" overwrites existing files; "false" for third arg means
+    // do not maintain entry order (faster), "true" for last would preserve permissions.
+    zip.extractAllTo(destination, true);
+}
+
+/**
+ * @function DownloadFiles Download Runtime Files.
  * @param {*} runtimePaths Runtime Paths.
  * @param {*} destinations Destination Paths.
  */
 function DownloadFiles(runtimePaths, destinations) {
     const filePath = `${__dirname}`;
 
-    for (i = 0; i < runtimePaths.length; i++) {
+    for (let i = 0; i < runtimePaths.length; i++) {
         let runtimePath = runtimePaths[i];
         let destination = destinations[i];
-        dl = new DownloaderHelper(runtimePath, filePath);
+        let zipFileName = runtimePath.slice(runtimePath.lastIndexOf('/') + 1);
+        let zipFullPath = path.join(filePath, zipFileName);
+
+        let dl = new DownloaderHelper(runtimePath, filePath);
         dl.on('end', () => {
             console.log("Downloaded from " + runtimePath + ".");
             console.log(destination);
-            decompress(filePath + "/" + runtimePath.slice(runtimePath.lastIndexOf('/') + 1), destination)
-            .then((files) => {
+            try {
+                ExtractZip(zipFullPath, path.join(filePath, destination));
                 console.log("Extracted " + destination + ".");
-                fs.unlink(filePath + "/" + runtimePath.slice(runtimePath.lastIndexOf('/') + 1), (err) => {
-                    if (err) {
-                        throw err;
-                    }
-                
-                    console.log("Deleted Zip successfully.");
-                });
-            })
-            .catch((error) => {
-                console.log("Error extracting " + filePath + "/"
-                + runtimePath.slice(runtimePath.lastIndexOf('/') + 1) + ": " + error + ".");
-                fs.unlink(filePath + "/" + runtimePath.slice(runtimePath.lastIndexOf('/') + 1), (err) => {
-                    if (err) {
-                        throw err;
-                    }
-                
-                    console.log("Deleted Zip successfully.");
-                });
+            } catch (error) {
+                console.log("Error extracting " + zipFullPath + ": " + error + ".");
+            }
+            fs.unlink(zipFullPath, (err) => {
+                if (err) {
+                    console.log("Error deleting zip: " + err);
+                    return;
+                }
+                console.log("Deleted Zip successfully.");
             });
+        });
+        dl.on('error', (err) => {
+            console.log("Download error for " + runtimePath + ": " + err);
         });
         dl.start();
     }
@@ -49,9 +63,9 @@ function DownloadFiles(runtimePaths, destinations) {
 if (process.argv.length < 4) {
     console.log("Error: At least one runtime download path and destination is required.");
 } else {
-    runtimes = [];
-    destinations = [];
-    for (i = 2; i < process.argv.length; i+= 2) {
+    let runtimes = [];
+    let destinations = [];
+    for (let i = 2; i < process.argv.length; i += 2) {
         runtimes.push(process.argv[i]);
         destinations.push(process.argv[i + 1]);
     }
